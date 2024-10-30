41d7bd4181aed5f38e945a8d16292d951773b798
Emilia Glibowska <emilia.glibowska@elmark.com.pl>
2024-10-30 14:34:36 +0100

    Solved day 2

--- __main__.py
+++ __main__.py
@@ -1,19 +1,18 @@
 import asyncio
+from types import ModuleType
 from day_01 import solution as day_01_solution
 from day_02 import solution as day_02_solution
 
-async def main():
-    print(f"{' Day 1 ':-^80}")
-    with open("day_01/input.txt") as f:
+async def run_day(day: int, solution: ModuleType):
+    print(f"{f' Day {day} ':-^80}")
+    with open(f"inputs/day_{day:02}") as f:
         data = f.read().splitlines()
-    print(await day_01_solution.run_part_one(data))
-    print(await day_01_solution.run_part_two(data))
+    print(await solution.run_part_one(data))
+    print(await solution.run_part_two(data))
 
-    print(f"{' Day 2 ':-^80}")
-    with open("day_02/input.txt") as f:
-        data = f.read().splitlines()
-    print(await day_02_solution.run_part_one(data))
-    print(await day_02_solution.run_part_two(data))
+async def main():
+    await run_day(1, day_01_solution)
+    await run_day(2, day_02_solution)
 
 
 if __name__ == "__main__":
--- day_02/solution.py
+++ day_02/solution.py
@@ -4,7 +4,7 @@ async def run_part_one(input_data: list[str]) -> int:
     max_red = 12
     max_green = 13
     max_blue = 14
-    impossible_id_sum = 0
+    possible_id_sum = 0
     for line in input_data:
         game_id = re.findall(r"Game (\d+):", line)[0]
         reveals_string = line.split(":")[1].strip()
@@ -17,9 +17,31 @@ async def run_part_one(input_data: list[str]) -> int:
             blue = re.findall(r"(\d+) blue", reveal)
             blue = int(blue[0]) if blue else 0
             if red > max_red or green > max_green or blue > max_blue:
-                impossible_id_sum += int(game_id)
-    return impossible_id_sum
+                break
+        else:
+            possible_id_sum += int(game_id)
+
+    return possible_id_sum
 
 
 async def run_part_two(input_data: list[str]) -> int:
-    return 0
+    power_sum = 0
+    for line in input_data:
+        min_red = 0
+        min_green = 0
+        min_blue = 0
+        reveals_string = line.split(":")[1].strip()
+        reveals = reveals_string.split(";")
+        for reveal in reveals:
+            red = re.findall(r"(\d+) red", reveal)
+            red = int(red[0]) if red else 0
+            green = re.findall(r"(\d+) green", reveal)
+            green = int(green[0]) if green else 0
+            blue = re.findall(r"(\d+) blue", reveal)
+            blue = int(blue[0]) if blue else 0
+            min_red = max(min_red, red)
+            min_green = max(min_green, green)
+            min_blue = max(min_blue, blue)
+        power_sum += min_red * min_green * min_blue
+
+    return power_sum
